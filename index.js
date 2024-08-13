247f9b2c354723d623642b5878f2805d56cb1fb2
Anne2337 <atuyishime28@gmail.com>
2024-08-13 20:58:32 +0200

    temperature conversion

--- index.js
+++ index.js
@@ -68,30 +68,92 @@
 //z=Math.sqrt(x);
 //console.log(z);
 
-const mastercard=document.getElementById("masterbtn");
-const paypal=document.getElementById("paypal");
-const mycheck=document.getElementById("check");
-const submit=document.getElementById("sumit");
-const result=document.getElementById("subresult");
-const payment=document.getElementById("paymentresult");
+//const mastercard=document.getElementById("masterbtn");
+//const paypal=document.getElementById("paypal");
+//const mycheck=document.getElementById("check");
+//const submit=document.getElementById("sumit");
+//const result=document.getElementById("subresult");
+//const payment=document.getElementById("paymentresult");
 
-submit.onclick=function(){
-    if(mycheck.checked){
-        result.textContent='you are subcribed!..';
+//submit.onclick=function(){
+    //if(mycheck.checked){
+       // result.textContent='you are subcribed!..';
 
-    }
-    else{
-        result.textContent="you are not subcribed";
-    }
-    if(paypal.checked){
-        payment.textContent="you are paying with paypal!...";
-    }
-    else if(mastercard.checked){
-        payment.textContent="you are paying with mastercard!...";
+    //}
+    //else{
+       // result.textContent="you are not subcribed";
+    //}
+    //if(paypal.checked){
+       // payment.textContent="you are paying with paypal!...";
+    //}
+    //else if(mastercard.checked){
+        //payment.textContent="you are paying with mastercard!...";
 
 
+    //}
+    //else{
+       // payment.textContent="you must select payment method";
+    //}
+//}
+
+
+//switch case
+ //let day=9;
+ //switch(day){
+   // case 1:
+        //console.log("monday");
+       // break;
+
+    //case 2:
+       // console.log("Tuesday");
+       // break;
+
+   // case 3:
+       // console.log("wensday");
+       // break;
+
+   // case 4:
+       // console.log("thusday");
+       // break;
+
+   // case 5:
+       // console.log("friday");
+       // break;
+
+   // case 6:
+       // console.log("sartudY");
+       // break;
+
+   // case 7:
+       // console.log("sunday");
+       // break;
+        //default:
+       // console.log(`${day} this day is not exist`);
+    //}
+
+    const textbox=document.getElementById("textbox");
+    const fargheigt=document.getElementById("fargheigt");
+    const celsius=document.getElementById("celsius");
+    const result=document.getElementById("result");
+     let temp;
+
+
+    
+
+ function convert(){
+    if(fargheigt.checked){
+       temp= Number(textbox.value);
+       temp= temp * 9 / 5 + 32;
+       result.textContent= temp + "°F";
     }
-    else{
-        payment.textContent="you must select payment method";
-    }
-}
+    else if (celsius.checked){
+        temp= Number(textbox.value);
+       temp= (temp-32)*(5/9);
+       result.textContent= temp + "°c";
+        
+          }
+          else{
+            result.textContent="you select unit";
+          }
+
+ }
